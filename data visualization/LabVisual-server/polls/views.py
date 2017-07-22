f3e1940cfd217577412168e21369ef4fbe57389e
Wizna <12302010012@fudan.edu.cn>
2017-07-22 03:08:17 +0200

    clean code and added comments

--- data visualization/LabVisual-server/polls/views.py
+++ data visualization/LabVisual-server/polls/views.py
@@ -1,22 +1,13 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
-
-# def index(request):
-#     return HttpResponse("Hello, world. You're at the polls index.")
-
 from django.http import JsonResponse
 from sklearn import preprocessing
 import MySQLdb
 import numpy as np
-from django.core import serializers
-import codecs, json
 from scipy import cluster
 import pandas as pd
 
 
 def index(request):
+    # get desired etest number from query
     testNum = request.GET.get('test')
     # Connect with the MySQL Server
     db = MySQLdb.connect(host='dolgi.informatik.rwth-aachen.de', port=3306, user='ldavlab_ruiming',
@@ -27,26 +18,27 @@ def index(request):
 
     # execute SQL query using execute() method.
     cursor.execute(
-        'SELECT Bewertung, IF(VerbrauchteZeit = 0, SpentTime/60, VerbrauchteZeit/60) as amount    FROM     ldavlab_ruiming.etesttries_view where      etestnr = '+testNum+' and Status = "Beendet" and SpentTime < 3600 group by Nachname')
+        'SELECT Bewertung, IF(VerbrauchteZeit = 0, SpentTime/60, VerbrauchteZeit/60) as amount    FROM     ldavlab_ruiming.etesttries_view where      etestnr = ' + testNum + ' and Status = "Beendet" and SpentTime < 3600 group by Nachname')
 
     data = cursor.fetchall()
     db.close()
 
+    # transform data to data frame
     X = np.array(data)
     data = preprocessing.scale(X)
-    # print(data)
     df = pd.DataFrame([[ij for ij in i] for i in data])
     df.rename(columns={0: 'Grade', 1: 'Time'}, inplace=True);
 
-    x = df['Time']
-    y = df['Grade']
-
+    # doing k-means clustering
     initial = [cluster.vq.kmeans(data, i) for i in range(1, 10)]
 
+    # k set to 4 was decided by analysing the decreasing graph of inner covariance
     cent, var = initial[3]
     # use vq() to get as assignment for each obs.
     assignment, cdist = cluster.vq.vq(data, cent)
-    # df = df.assign(Class=pd.Series(assignment).values)
+    # use the original data to show final results instead of the normalized ones
     dn = pd.DataFrame([[ij for ij in i] for i in X])
     dn = dn.assign(Class=pd.Series(assignment).values)
-    return JsonResponse(dn.to_json(orient='records'), safe= False)
+
+    # return data in json format
+    return JsonResponse(dn.to_json(orient='records'), safe=False)
--- data visualization/LabVisual-server/sankey/views.py
+++ data visualization/LabVisual-server/sankey/views.py
@@ -1,32 +1,33 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
 import re
 import pickle
 import operator
 from django.http import JsonResponse
-import json
 
-data = pickle.load( open( "sankey/coursetable_jan.p", "rb" ) )
+# read in students selection of courses data in march
+data = pickle.load(open("sankey/coursetable_march.p", "rb"))
 listforfp = list(data.values())
-lv2name = pickle.load( open( "sankey/lv2name.p", "rb" ) )
+# thanks for your advice, after analyzing the numbers of courses map to a single ip
+# I decided to use those less than 16 as valid ones after doing a exponential regression
+listforfp = [item for item in listforfp if len(item) < 16]
+# read in dictionary of (LVNumber, course name)
+lv2name = pickle.load(open("sankey/lv2name.p", "rb"))
+
 
 def index(request):
+    # get list of constraint courses
     courses = request.GET.get('course')
-    print(courses)
-    strarray = re.findall('[0-9]{2}\w[sS]\-[0-9]{5}',courses)
-    strarray = [int(i.lower().replace('ws-','1').replace('ss-','2')) for i in strarray]
-    print(strarray)
+    strarray = re.findall('[0-9]{2}\w[sS]\-[0-9]{5}', courses)
+    strarray = [i.lower().replace('ws-', '1').replace('ss-', '2') for i in strarray]
 
-    size = len(strarray)
+    # remove duplicate ones
     queryset = set(strarray)
 
+    # decide the popularity of courses for further sorting
     dictsort = {}
     for courses in listforfp:
         if queryset.issubset(courses):
             for cornum in courses:
-
+                # check whether to accumulate on existed one or set for the first time
                 if cornum in dictsort:
                     dictsort[cornum] += 1
                 else:
@@ -34,60 +35,69 @@ def index(request):
 
     sorted_dict = sorted(dictsort.items(), key=operator.itemgetter(1), reverse=True)
     orderedlist = [int(i[0]) for i in sorted_dict]
-    # to make first one not counted, the selected one
-    orderedlist = orderedlist[size:200]
+    # limit the number of courses considered, also remove the query courses
+    orderedlist = orderedlist[:100]
+    for item in queryset:
+        orderedlist.remove(int(item))
     orderedset = set(orderedlist)
-    newfp = []
-    for item in listforfp:
-        templist = list(set(item) & orderedset)
-        templist.sort(key=lambda x: orderedlist.index(x))
-        newfp.append(templist)
 
+    pattern = re.compile('^[0-9]+$')
+
+    newfp = []
+    # make only those contains constraint course count
+    for index, item in enumerate(listforfp):
+        # add the check to make other connection not counting
+        if queryset.issubset(item):
+            temp = [int(i) for i in item if pattern.match(i)]
+            newfp.append(list(set(temp) & orderedset))
+
+    # After sorting, I am now able to construct a frequent pattern tree
+    # although I actually merge all nodes with same names in the graph
+    # and make it not a tree
+    for item in newfp:
+        item.sort(key=lambda x: orderedlist.index(x))
+
+    # accumulate link for each adjacent pairs of courses
     dictlink = {}
     for item in newfp:
         for index, subitem in enumerate(item):
             if index != 0:
                 insertLink(item[index - 1], subitem, dictlink)
 
+    # sort links by popularity and select the most important ones
     sortedlink = sorted(dictlink.items(), key=operator.itemgetter(1), reverse=True)
-    sortedlink = sortedlink[:120]
-    # print(sortedlink)
+    sortedlink = sortedlink[:100]
+
+    # remove duplicate course names if there are
+    # (to make nodes can be identified by course names)
     checkDuplicate = []
     containCourse = []
-
     for key, value in sortedlink:
         if key[0] in lv2name:
             temp1 = lv2name[key[0]]
             if temp1 not in checkDuplicate:
                 containCourse.append(key[0])
                 checkDuplicate.append(temp1)
-
         if key[1] in lv2name:
             temp2 = lv2name[key[1]]
-
             if temp2 not in checkDuplicate:
                 containCourse.append(key[1])
                 checkDuplicate.append(temp2)
-
     orderedlist = list(orderedset & set(containCourse))
-
     sortedlink = [(key, value) for key, value in sortedlink if key[0] in orderedlist and key[1] in orderedlist]
 
-    print(len(orderedlist))
-
+    # prepare data in json format
     dictforjson = {}
     datajson = [{"source": lv2name[key[0]], "target": lv2name[key[1]], "value": value} for key, value in sortedlink if
                 key[0] in lv2name and key[1] in lv2name]
     namejson = [{"name": lv2name[course]} for course in orderedlist if course in lv2name]
-    # datajson = [{"source": str(key[0]), "target": str(key[1]), "value": value} for key, value in sortedlink]
-    # namejson = [{"name": str(course)} for course in orderedlist]
     dictforjson["nodes"] = namejson
     dictforjson["links"] = datajson
 
-    print(dictforjson)
     return JsonResponse(dictforjson)
 
 
+# function to accumulate links number
 def insertLink(source, target, dictionary):
     indextuple = (source, target)
     if indextuple in dictionary:
